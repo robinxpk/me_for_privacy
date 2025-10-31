0307e2b006b930fd393ecae715db8ac6d947ac96
Lona <lona.koers@gmail.com>
2025-10-31 18:23:45 +0100

    comments for interactive

--- code/playingaround.py
+++ code/playingaround.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
+# %%
 nhanes_data_path = r"data/DEMO_L.xpt"
 
 nhanes = pd.read_sas(nhanes_data_path)
 
 
-
 print("Done")
 
+# %
+# nhanes_rename_dict = {
+#     # Format: {"newname": "oldname", ...}
+#     "SEQN": "id"
+# }
+# nhanes.rename(
+#     index = nhanes_rename_dict
+# )
 
-
-
-nhanes_rename_dict = {
-    # Format: {"newname": "oldname", ...}
-    "SEQN": "id"
-}
-nhanes.rename(
-    index = nhanes_rename_dict
-)
+# %%
